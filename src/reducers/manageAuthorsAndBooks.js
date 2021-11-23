0c8484336503bf080bb0f8f21baf27462c8af835
Ben Spotswood <spotswoodb@gmail.com>
2021-11-23 12:32:34 -0600

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,43 +1,57 @@
-export default function bookApp(
-  state = {
-    authors: [],
-    books: []
-  },
-  action
-) {
-  let idx;
-  switch (action.type) {
-    case "ADD_BOOK":
-      return {
-        ...state,
-        authors: [...state.authors],
-        books: [...state.books, action.book]
-      };
+// import { combineReducers } from "redux";
+// import uuid from "uuid";
+
+// const rootReducer = combineReducers({
+//   authors: authorsReducer,
+//   books: booksReducer
+// })
+
+// export default rootReducer;
+
+
+
+
+
+// // export default function bookApp(
+// //   state = {
+// //     authors: [],
+// //     books: []
+// //   },
+// //   action
+// // ) {
+// //   let idx;
+// //   switch (action.type) {
+// //     case "ADD_BOOK":
+// //       return {
+// //         ...state,
+// //         authors: [...state.authors],
+// //         books: [...state.books, action.book]
+// //       };
  
-    case "REMOVE_BOOK":
-      idx = state.books.findIndex(book => book.id === action.id);
-      return {
-        ...state,
-        authors: [...state.authors],
-        books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
-      };
+// //     case "REMOVE_BOOK":
+// //       idx = state.books.findIndex(book => book.id === action.id);
+// //       return {
+// //         ...state,
+// //         authors: [...state.authors],
+// //         books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
+// //       };
  
-    case "ADD_AUTHOR":
-      return {
-        ...state,
-        books: [...state.books],
-        authors: [...state.authors, action.author]
-      };
+// //     case "ADD_AUTHOR":
+// //       return {
+// //         ...state,
+// //         books: [...state.books],
+// //         authors: [...state.authors, action.author]
+// //       };
  
-    case "REMOVE_AUTHOR":
-      idx = state.authors.findIndex(author => author.id === action.id);
-      return {
-        ...state,
-        books: [...state.books],
-        authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
-      };
+// //     case "REMOVE_AUTHOR":
+// //       idx = state.authors.findIndex(author => author.id === action.id);
+// //       return {
+// //         ...state,
+// //         books: [...state.books],
+// //         authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
+// //       };
  
-    default:
-      return state;
-  }
-}
+// //     default:
+// //       return state;
+// //   }
+// // }
